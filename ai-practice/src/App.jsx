7d180cf7f884590c826f7366c46c88fd729931a0
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-04-20 22:57:08 +0900

    :sparkles: created image generator

--- ai-practice/src/App.jsx
+++ ai-practice/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPEN_AI_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+
+    setResult(res.data.data[0].url);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="app-main">
+        <h1>Generate an image using Open AI API</h1>
+        <input
+          className="app-input"
+          placeholder="Type something to generate an image.."
+          onChange={(e) => {
+            setPrompt(e.target.value);
+          }}
+        />
+        <button onClick={generateImage}>Generate an image</button>
 
-export default App
+        <img src={result} alt="generatedImg" />
+      </div>
+    </>
+  );
+};
+
+export default App;
